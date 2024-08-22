d2b2e79680c1aa16f8ae87b36a4cbafcfaf52251
montanarisofia <162723443+montanarisofia@users.noreply.github.com>
2024-08-22 00:36:20 -0300

    exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,17 @@
 def math():
     a = 57
     b = 7
+    #SUMA
+    print(a+b)
+    #DIFERENCIA
+    print(a-b)
+    #PRODUCTO
+    print(a*b)
+    #PROMEDIO
+    print((a+b)/2)
+    #COCIENTE ENTERO
+    print(a//b)
+    #RESTO DE LA DIVISION ENTERA
+    print(a%b)
+    #VALOR REAL DE LA DIVISION
+    print(a/b)
